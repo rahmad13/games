c543fbd29a8969669b0478cff5eea0bdd71bf561
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-01 18:30:43 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler =  m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [085713964963]
-│ • Telkomsel [081319944687]
+│ • Telkomsel [082328303332]
 ╰────
 
 ╭─「 Donasi • Non Pulsa 」
-│ • https://saweria.co/BochilGaming
-│ • Gopay [085713964963]
+│ • Ovo [082328303332]
+│ • Paypal []
+│ • Dana [082328303332]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
