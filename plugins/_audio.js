22c880ac94131d71b95209bec7104c1a1b7c16c7
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-04 13:58:09 +0700

    Create _audio.js

---
+++ plugins/_audio.js
@@ -0,0 +1,13 @@
+import fs from 'fs'
+
+let handler = async (m, { text, usedPrefix, command }) => {
+
+var cuk = fs.readFileSync('../src/audio/nBWefmc.mp3')
+
+conn.sendFile(m.chat, cuk, null, null, m, null, {
+    asDocument: chat.useDocument
+  })
+}
+handler.customPrefix = /^(bot|hyung|seok)/i
+handler.command = new RegExp
+export default handler
