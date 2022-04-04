df0a16e39b27b58424d4c040f371c81aaa0a71ef
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-04 14:02:11 +0700

    Update _audio.js

--- plugins/_audio.js
+++ plugins/_audio.js
@@ -3,6 +3,7 @@ import fs from 'fs'
 let handler = async (m, { text, usedPrefix, command }) => {
 
 var cuk = fs.readFileSync('../src/audio/nBWefmc.mp3')
+let chat = global.db.data.chats[m.chat]
 
 conn.sendFile(m.chat, cuk, null, null, m, null, {
     asDocument: chat.useDocument
