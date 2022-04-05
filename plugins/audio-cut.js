976a5e0ecb7a47a494a6215513cb8b5fc8e3ad07
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-05 10:33:28 +0700

    Update audio-cut.js

--- plugins/audio-cut.js
+++ plugins/audio-cut.js
@@ -4,17 +4,25 @@ import { exec } from 'child_process'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) return m.reply('detik?')
     if (!args[1]) return m.reply('detik?')
+
+try {
      let q = m.quoted ? m.quoted : m
      let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-    if (!/audio/.test(mime)) throw 'reply audio'
+    if (!/audio/.test(mime)) {
      let media = await q.download?.()
      if (!media) throw 'Can\'t download media'
      let ran = getRandom('.mp3')
      exec(`ffmpeg -ss ${args[0]} -i ${media} -t ${args[1]} -c copy ${ran}`, (err, stderr, stdout) => {
+     fs.unlinkSync(media)
+     if (err) m.reply(`_*Error!*_`)
      let buff = fs.readFileSync(ran)
      conn.sendFile(m.chat, buff, ran, null, m, true, { quoted: m, mimetype: 'audio/mp4' })
      fs.unlinkSync(ran)
          })
+      } else m.reply(`Reply Audio!!!`)
+    } catch (e) {
+      m.reply(e)
+         }
        }
 handler.help = ['cut (reply|second|to second)']
 handler.tags = ['audio']
