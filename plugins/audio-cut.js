6230b9baaa022da194ea1a471ac420b8099a3747
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-05 08:09:54 +0700

    Update audio-cut.js

--- plugins/audio-cut.js
+++ plugins/audio-cut.js
@@ -11,7 +11,6 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
      if (!media) throw 'Can\'t download media'
      let ran = getRandom('.mp3')
      exec(`ffmpeg -ss ${args[0]} -i ${media} -t ${args[1]} -c copy ${ran}`, (err, stderr, stdout) => {
-     fs.unlinkSync(media)
      let buff = fs.readFileSync(ran)
      conn.sendFile(m.chat, buff, ran, null, m, true, { quoted: m, mimetype: 'audio/mp4' })
      fs.unlinkSync(ran)
