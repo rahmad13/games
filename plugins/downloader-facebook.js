9790c05336c93e4ce3f04685108c9299ff7e73c2
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-11 13:27:49 +0700

    Update downloader-facebook.js
    no

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,8 +1,13 @@
 import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://fb.watch/azFEBmFRcy/`
+    
+try {
     const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
     for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `🔗 *Url:* ${url}`, m)
+  } catch (e) {
+   m.reply(`Eror saat mengambil data`)
+   }
 }
 handler.help = ['facebbok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
