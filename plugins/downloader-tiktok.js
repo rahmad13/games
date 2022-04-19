18ac3ee7aecb29469cefc115dde6e28ae9784664
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-19 20:32:58 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,6 +1,8 @@
 import { tiktokdl, tiktokdlv2 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+
+     try {
     const { author: { nickname }, video, description } = await tiktokdl(args[0]).catch(async _ => await tiktokdlv2(args[0]))
     const url = video.no_watermark_raw || video.no_watermark || video.no_watermark_hd || video.with_watermark 
     if (!url) throw 'Can\'t download video!'
@@ -8,6 +10,9 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 🔗 *Url:* ${url}
 🧏 *Nickname:* ${nickname}${description ? `🖹 *Description:* ${description}` : ''}
 `.trim(), m)
+   } catch (e) {
+     m.reply("Server Down/Eror Please try again")
+   }
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
