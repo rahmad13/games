2f557a16bd1be7e4db85e3d43fda8805d0695fec
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-20 11:25:43 +0700

    Update sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -4,7 +4,7 @@ import uploadImage from '../lib/uploadImage.js'
 import { webp2png } from '../lib/webp2mp4.js'
 
 
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!m.quoted) throw 'Quoted the sticker!'
   let stiker = false
   try {
@@ -14,17 +14,16 @@ let handler = async (m, { conn, text }) => {
     let mime = m.quoted.mimetype || ''
     if (/webp/.test(mime)) {
       let img = await m.quoted.download()
-      let out = await uploadFile(img)
-      stiker = await sticker( out, packname || '', author || '')
+      stiker = await addExif(img, packname || '', author || '')
     } else if (/image/.test(mime)) {
-      let img = await m.quoted.download()
-      let out = await uploadImage(img)
-      stiker = await sticker( out, packname || '', author || '')
+      let imge = await m.quoted.download()
+      let outi = await uploadImage(imge)
+      stiker = await sticker(outi, packname || '', author || '')
     } else if (/video/.test(mime)) {
       if ((q.msg || q).seconds > 11) return m.reply('maks 10 detik!')
-      let img = await m.quoted.download()
-      let out = await uploadImage(img)
-      stiker = await sticker( out, packname || '', author || '')
+      let imgo = await m.quoted.download()
+      let outu = await uploadImage(imgo)
+      stiker = await sticker( outu, packname || '', author || '')
     }
   } finally {
     if (stiker) await conn.sendFile(m.chat, stiker, 'stiker.webp', '', m, false, { asSticker: true })
