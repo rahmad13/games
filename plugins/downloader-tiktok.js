64229c8ec4161511db00f501758544e7580fbb61
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-21 05:12:38 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,4 +1,4 @@
-import { tiktok } from '../lib/tiktok.js'
+import tiktok from '../lib/tiktok.js'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
