e586d4b26c0392a2dfc507cf72c0eccd4a462623
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-21 11:42:55 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,13 +1,22 @@
-import tiktok from '../lib/tiktok.js'
-
+import { tiktokdl, tiktokdlv2 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    const tt = await tiktok(args[0])
- conn.sendFile(m.chat, tt.result.nowm , 'tiktok.mp4', null, m)
+
+     try {
+    const { author: { nickname }, video, description } = await tiktokdl(args[0])
+    const url = video.no_watermark_raw || video.no_watermark || video.no_watermark_hd || video.with_watermark 
+    if (!url) throw 'Can\'t download video!'
+    conn.sendFile(m.chat, url, 'tiktok.mp4', `
+🔗 *Url:* ${url}
+🧏 *Nickname:* ${nickname}${description ? `🖹 *Description:* ${description}` : ''}
+`.trim(), m)
+   } catch (e) {
+     m.reply("Server Down/Eror Please try again")
+   }
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
-handler.command = /^(tik(tok)?(dl)?)$/i
+handler.command = /^(tik(tok)?(dl)?(no)?(wm)?)$/i
 
 export default handler
