cee443924d2b06468c09c24b028588a9e8692767
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-23 04:26:59 +0700

    Create downloader-savefrom.js

---
+++ plugins/downloader-savefrom.js
@@ -0,0 +1,16 @@
+import { aiovideodl, savefrom } from ''@bochilteam/scraper'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+   // const { result } = await tiktokvid(args[0]).catch(async _ => await musicaldown(args[0]))
+   // const done = result.nowatermark || result.watermark || result.video || result.video_original
+   // if (!done) throw 'Can\'t download video!'
+   // conn.sendFile(m.chat, done, '', `🔗 *Url:* ${done}`, m)
+   console.log(await savefrom(args[0]))
+}
+handler.help = ['savefrom'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+
+handler.command = /^(save(from)?(dl)?(aio)?(video)?)$/i
+
+export default handler
