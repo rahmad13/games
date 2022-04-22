c21d85bf3156135df82b13dc2b97ee32a3e6d0b1
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-23 05:03:01 +0700

    Update downloader-zippy.js

--- plugins/downloader-zippy.js
+++ plugins/downloader-zippy.js
@@ -5,10 +5,14 @@ import { zippydl} from '../lib/scrape.js'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
    const { hasil } = await zippydl(args[0])
-    const done = hasil.link
+   const done = hasil.link
+   const limitedSize = (limit) * 1024
+   var isLimit = limitedSize < hasil.fileSize
+   m.reply(isLimit ? 'Size terlalu besar download sendiri' + done : wait)
    if (!done) throw 'cant download'
-   conn.sendFile(m.chat, done, hasil.title, `🔗 *Url:* ${done}`, m)
+   if (!isLimit) await conn.sendFile(m.chat, done, hasil.title, `🔗 *Url:* ${done}`, m, null, { asDocument: true })
    
+}
 handler.help = ['zippyshare'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
