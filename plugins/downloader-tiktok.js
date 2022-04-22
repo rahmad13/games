c8a0f6659f59e4cff368275eaa499ed6b39dfae9
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-22 20:27:29 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,16 +1,14 @@
-import { tiktokdl, tiktokdlv2 } from '@bochilteam/scraper'
+import { Tiktokdownloader } from '../lib/tiktokdl.js'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
 
      try {
-    const { author: { nickname }, video, description } = await tiktokdl(args[0]).catch(async _ => await tiktokdlv2(args[0]))
-    const url = video.no_watermark_raw || video.no_watermark || video.no_watermark_hd || video.with_watermark 
-    if (!url) throw 'Can\'t download video!'
-    conn.sendFile(m.chat, url, 'tiktok.mp4', `
-🔗 *Url:* ${url}
-🧏 *Nickname:* ${nickname}${description ? `🖹 *Description:* ${description}` : ''}
-`.trim(), m)
+    const tt = await Tiktokdownloader(args[0])
+    if (!tt.result.status) throw 'Can\'t download video!'
+    conn.sendFile(m.chat, tt.result.nowatermark, 'tiktok.mp4', `
+🔗 *Url:* ${tt.result.nowatermark}
+`, m)
    } catch (e) {
      m.reply("Server Down/Eror Please try again")
    }
