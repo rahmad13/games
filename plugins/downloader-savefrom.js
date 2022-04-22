0927b14592f5e3a1888660851a2c7916fc813013
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-23 05:17:05 +0700

    Update downloader-savefrom.js

--- plugins/downloader-savefrom.js
+++ plugins/downloader-savefrom.js
@@ -2,11 +2,13 @@ import { aiovideodl, savefrom } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-   // const { result } = await tiktokvid(args[0]).catch(async _ => await musicaldown(args[0]))
-   // const done = result.nowatermark || result.watermark || result.video || result.video_original
-   // if (!done) throw 'Can\'t download video!'
-   // conn.sendFile(m.chat, done, '', `🔗 *Url:* ${done}`, m)
-   console.log(await savefrom(args[0]))
+const { meta, hd, sd} = await savefrom('https://www.facebook.com/100048678392753/posts/526563552309587/?app=fbl')
+
+const done = hd.url || sd.url
+ conn.sendFile(m.chat, done, `savefrom.mp4`, ` *👤 Profil:* ${meta.title}
+*⌛ durasi:* ${meta.duration}
+🔗 *Url:* ${done}`, m)
+
 }
 handler.help = ['savefrom'].map(v => v + ' <url>')
 handler.tags = ['downloader']
