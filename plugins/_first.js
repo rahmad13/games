88afb24406826d36c8671c16018ece6fb013afc7
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-22 12:07:32 +0700

    Update _first.js

--- plugins/_first.js
+++ plugins/_first.js
@@ -4,7 +4,7 @@ if (m.chat.endsWith('broadcast') || m.fromMe || m.isGroup || db.data.settings[th
     let user = global.db.data.users[m.sender]
     if (new Date - user.pc < 86400000) return // setiap 24 jam sekali
     await this.sendHydrated(m.chat, `
-Hai, ${this.ucapan()}
+Hai, ${this.getName(m.sender)}
 
 ${user.banned ? 'kamu dibanned' : `Ada yang bisa ${this.user.name} bantu?`}
 `.trim(), author, null, webpriv, "Website", null, null, [[user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : ',?']], m)
