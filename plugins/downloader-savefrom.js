82c25a63e968c57dccb830e347fc05457b19a32e
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-23 10:50:53 +0700

    Update downloader-savefrom.js

--- plugins/downloader-savefrom.js
+++ plugins/downloader-savefrom.js
@@ -2,7 +2,7 @@ import { aiovideodl, savefrom } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.facebook.com/groups/972938613264044/permalink/1103753026849268/?app=fbl`
-const { meta, hd, sd} = await savefrom(args[0])
+const {Author: url, meta, hd, sd} = await savefrom(args[0]).catch(async _ => await aiovideodl(args[0]))
 
 const done = hd.url || sd.url
  conn.sendFile(m.chat, done, `savefrom.mp4`, ` *👤 Profil:* ${meta.title}
