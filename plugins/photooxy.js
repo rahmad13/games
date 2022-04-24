97074e1173ef50a8b5fdbe9e993a6124c05fc16e
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-24 17:58:29 +0700

    Create photooxy.js

---
+++ plugins/photooxy.js
@@ -0,0 +1,33 @@
+import {
+pShadow,
+  pRomantic,
+  pSmoke,
+  pBurnPapper,
+  pNaruto,
+  pLoveMsg,
+  pMsgGrass,
+  pGlitch,
+  pDoubleHeart,
+  pCoffeCup,
+  pLoveText,
+  pButterfly
+} from '../lib/photooxy.js'
+
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+
+if (!args[1]) throw 'textnya coy'
+
+try {
+const {result} = await command(args[1])
+
+conn.sendFile(m.chat, result.url, 'potooxy.jpg', `*📎Url:* ${result.url}`, m)
+} catch (e) {
+m.reply(`Eror`)
+}
+
+}
+
+handler.help = ['pRomantic', 'pSmoke','pNaruto','pBurnPapper'].map(v => v + ' <text>')
+handler.tags = ['photooxy']
+handler.command = /^(p(Smoke)?(Romantic)?(Naruto)?(BurnPapper)?)$/i
+export default handler
