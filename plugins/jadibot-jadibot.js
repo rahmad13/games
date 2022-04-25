69af83dfef0036c0a5520c1abef3736ab14367a6
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-25 08:32:33 +0700

    Update jadibot-jadibot.js

--- plugins/jadibot-jadibot.js
+++ plugins/jadibot-jadibot.js
@@ -1,7 +1,41 @@
+const {
+  useSingleFileAuthState,
+  default: _makeWaSocket,
+    makeWALegacySocket,
+    proto,
+    downloadContentFromMessage,
+    jidDecode,
+    areJidsSameUser,
+    generateForwardMessageContent,
+    generateWAMessageFromContent,
+    WAMessageStubType,
+    extractMessageContent,
+  DisconnectReason
+} = await import('@adiwajshing/baileys')
+import { is } from 'cheerio/lib/api/traversing'
+import qrcode 'qrcode'
+import fs from 'fs'
+import path from'path'
+import { makeWASocket, protoType, serialize } from './lib/simple.js';
+import { createRequire } from "module"; // Bring in the ability to create the 'require' method
+import path, { join } from 'path'
+import { fileURLToPath, pathToFileURL } from 'url'
+import { platform } from 'process'
+
 
 let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
- 
-m.reply(`Fitur ini belum tersedia`)
+  
+    const { state, saveState } = useSingleFileAuthState('jadibot')
+    const connectionOptions = {
+     printQRInTerminal: true,
+     auth: state,
+  // logger: pino({ level: 'trace' })
+     }
+    const login = makeWASocket(connectionOptions)
+
+conn.on('qr', async qr => {
+    conn.sendFile(m.chat, connectionOptions.printQRInTerminal, 'qr.jpg', 'Scan qr ini', m)
+    })
  }
 handler.help = ['jadibot']
 handler.tags = ['jadibot']
