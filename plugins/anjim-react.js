167aedf08b38e3882772a0bff24eace1a8511b94
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-27 21:21:06 +0700

    Update anjim-react.js

--- plugins/anjim-react.js
+++ plugins/anjim-react.js
@@ -5,12 +5,12 @@ ReactionMessage
 let handler = async(m, { conn, usedPrefix, command, args }) => {
     if (!args[0]) throw `Masukkan emoticon nya.\n\nContoh: ${usedPrefix + command} 🇨🇳`
 
-    const react new proto.ReactionMessage.create({})
+    const react = new proto.ReactionMessage.create({})
             react.text: args[0],
             react.key: m.quoted.key
      
     
-    await conn.relayMessage(m.chat, ReactionMessage: react)
+    await conn.relayMessage(m.chat, ReactionMessage: react, m)
 }
 handler.command = /^(react)$/i
 handler.help = ['reaact']
