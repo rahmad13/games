ee81151f9dbba73a9eb82ef82860b02f9a8c5117
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-27 21:12:08 +0700

    Create _react.js

---
+++ plugins/_react.js
@@ -0,0 +1,18 @@
+import {
+ReactionMessage
+} from "@adiwajshing/baileys")
+
+let handler = async(m, { conn, usedPrefix, command, args }) => {
+    if (!args[0]) throw `Masukkan emoticon nya.\n\nContoh: ${usedPrefix + command} 🇨🇳`
+
+        react: {
+            text: args[0],
+            key: m.quoted.key
+        }
+    
+    await conn.relayMessage(m.chat, ReactionMessage: react, m)
+}
+handler.command = /^(react)$/i
+handler.help = ['reaact']
+handler.tags = ['tools']
+export default handler
