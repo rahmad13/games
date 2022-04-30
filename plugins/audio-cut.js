1af80ed49b974071b6a5422da5141906a595335f
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-30 08:45:14 +0700

    Update audio-cut.js

--- plugins/audio-cut.js
+++ plugins/audio-cut.js
@@ -12,12 +12,13 @@ try {
         if (!audio) throw 'Can\'t download audio!'
         if (!args[0]) throw 'angkanya mana contoh: reply .bass 1 10'
         if (!args[1]) throw 'angka nya mana'
+      
+       exec(`ffmpeg -ss ${args[0]} -i ${media} -t ${args[1]} -c copy ${filename}`, async (err) => {
         let ran = (new Date * 1) + '.mp3'
         let media = '../tmp/' + ran
         let filename = media + '.mp3'
         await promises.writeFile(media, audio)
-        exec(`ffmpeg -ss ${args[0]} -i ${media} -t ${args[1]} -c copy ${filename}`, async (err) => {
-            await promises.unlink(media)
+          await promises.unlink(media)
             if (err) return Promise.reject( `_*Error!*_`)
             let buff = await promises.readFile(filename)
             m.reply(wait)
