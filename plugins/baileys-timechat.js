e2b5218c00b3468427a9b400afc24cfbb4c78c67
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-04-30 18:35:01 +0700

    Update baileys-timechat.js

--- plugins/baileys-timechat.js
+++ plugins/baileys-timechat.js
@@ -5,11 +5,10 @@ if (!args[1]) throw `.setchattime 62823283033324 2022-04-30 15:30 hola`
 if (!args[2]) throw `.setchattime 62823283033324 2022-04-30 15:30 hola`
 if (!args[3]) throw `.setchattime 62823283033324 2022-04-30 15:30 hola`
 
-let obj
-obj = { tanggal: `${args[1]}`, jam: `${args[2]` }
+let obj = { tanggal: `${args[1]}`, jam: `${args[2]` }
 
 let date1 = + new Date()
-let date2 = +new Date(obj.tanggal + " " + obj.jam)
+let date2 = + new Date(obj.tanggal + " " + obj.jam)
 let teks = args[3]
 setTimeout(() => conn.reply( args[0] + "@s.whatsapp.net", text, m), date2 - date1)
 
