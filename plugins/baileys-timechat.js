1e56980afaee621517239e396e348321fd91081e
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-05-01 13:38:12 +0700

    Update baileys-timechat.js

--- plugins/baileys-timechat.js
+++ plugins/baileys-timechat.js
@@ -1,24 +1,23 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-if (!args[0]) throw `.setchattime 62823283033324 2022-04-30 15:30 hola`
-if (!args[1]) throw `.setchattime 62823283033324 2022-04-30 15:30 hola`
-if (!args[2]) throw `.setchattime 62823283033324 2022-04-30 15:30 hola`
-if (!args[3]) throw `.setchattime 62823283033324 2022-04-30 15:30 hola`
+if (!args[1]) throw `.setchattime 2022-04-30 15:30 hola`
+if (!args[2]) throw `.setchattime 2022-04-30 15:30 hola`
+if (!args[3]) throw `.setchattime 2022-04-30 15:30 hola`
 
-let obj = { tanggal: `${args[1]}`, jam: `${args[2]}` }
+let obj = { tanggal: `${args[0]}`, jam: `${args[1]}` }
 
 let date1 = + new Date()
 
 let date2 = + new Date(obj.tanggal + " " + obj.jam)
 
-let teks = args[3]
+let teks = args[2]
 
-setTimeout(() => conn.reply( args[0] + "@s.whatsapp.net", text, m), date2 - date1)
+setTimeout(() => conn.reply(m.chat, text, m), date2 - date1)
 
 }
 
-handler.help = ['setchattime'].map(v => v + ' <nomeryg mau dikirim chat|tanggal|jam|text>')
-handler.tags = ['baileys']
+handler.help = ['setchattime'].map(v => v + ' <tanggal|jam|text>')
+handler.tags = ['Baileys']
 
 handler.command = /^(set(chat)?(time)?)$/i
 handler.limit = false
