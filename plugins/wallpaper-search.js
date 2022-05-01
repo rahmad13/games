1c19b49edbfaa5fb0edb9e47cc2cbda570bdc63d
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-05-01 16:25:41 +0700

    Update wallpaper-search.js

--- plugins/wallpaper-search.js
+++ plugins/wallpaper-search.js
@@ -12,7 +12,7 @@ if (!text) return m.reply('Nyari apa?')
     let json = await res.json()
     if(json.total_match == 0) return m.reply(`Tidak dapat menemukan \"${text}\"!`)
     let img = json.wallpapers[Math.floor(Math.random() * json.wallpapers.length)]
-    await conn.sendFile(m.chat, img.url_image, null, `Nih kak, Wallpapernya udah terkirim`, m, 0, { thumbnail: Buffer.alloc(0) })
+    await conn.sendFile(m.chat, img.url_image, "walp.jpg", `Nih kak, Wallpapernya udah terkirim`, m, 0, { thumbnail: Buffer.alloc(0) })
   }
 
 handler.help = ['wallq <nama>', 'walls <search>']
