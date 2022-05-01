31272e44170f5386e31ab795d029575dd246a096
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-05-01 13:31:54 +0700

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -5,10 +5,12 @@ let handler = async (m, { conn, usedPrefix, text, isOwner }) => {
     if (!code) throw 'Link invalid'
     let res = await conn.groupAcceptInvite(code)
     expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
-    m.reply(`Berhasil join grup ${res}${expired ? ` selama ${expired} hari` : ''}`)
+    m.reply(`Berhasil join grup ${res}${expired ? ` selama ${expired} hari` : ''}`).then(() => {
     let chats = global.db.data.chats[res]
     if (!chats) chats = global.db.data.chats[res] = {}
     if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
+   })
+   conn.sendButton(res.gid, `${conn.getName(conn.user.jid)} Adalah Bot whatsapp Yang dibangun dengan NodeJs, diinvit oleh @${m.sender.split`@`[0]}`, author, [['Menu','.menu'], ['Donasi','.donasi']], m)
 }
 handler.help = ['join <chat.whatsapp.com>']
 handler.tags = ['premium']
