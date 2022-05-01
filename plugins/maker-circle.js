f2257b69b067ba4899dc3fda6854c70ecb502458
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-05-01 09:26:11 +0700

    Update maker-circle.js

--- plugins/maker-circle.js
+++ plugins/maker-circle.js
@@ -4,9 +4,11 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-Canvas.circle(await conn.profilePictureUrl(who).catch(_ => './src/avatar_contact.png')).then((p) => conn.sendFile(m.chat, p, 'pp.jpg', 'done\n Made with canvas', m))
+let pp = await conn.profilePictureUrl(who).catch(_ => './src/avatar_contact.png')
 
-handler.help = ['circle'].map(v => v + ' <@tag>')
+Canvas.circle(pp).then((p) => conn.sendFile(m.chat, p, 'pp.jpg', 'done\n Made with canvas', m))
+
+handler.help = ['circle'].map(v => v + ' <tag>')
 handler.tags = ['maker']
 
 handler.command = /^(cir(cle)?(le)?(cele)?(kel)?)$/i
