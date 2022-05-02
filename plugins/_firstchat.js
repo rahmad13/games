b13f9b03f51dafe34e4824230424553ae2d62af8
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-05-02 12:39:58 +0700

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -4,8 +4,7 @@ if (m.isBaileys && m.fromMe && m.isGroup)
 let user = global.db.data.users[m.sender]
     if (new Date - user.pc < 86400000) return // setiap 24 jam sekali
     await this.sendButton(m.chat, `
-Hai, ${this.ucapan()}
-${user.banned ? 'kamu dibanned' : `Ada yang bisa ${this.user.name} bantu?`}
+Hai, ${user.banned ? 'kamu dibanned' : `Ada yang bisa ${this.user.name} bantu?`}
 `.trim(), author, [[user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : ',?']], m)
     user.pc = +new Date * 1
 }
