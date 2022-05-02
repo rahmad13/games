1dc6d9c7e2883c257d76dc7acfd9f64dcca26ef1
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-05-02 17:23:02 +0700

    Update jadian-tolak.js

--- plugins/jadian-tolak.js
+++ plugins/jadian-tolak.js
@@ -1,9 +1,4 @@
-/*//////////////////////////////////
-
-Cretor : Hairul Lana
-https://github.com/hairullana 
-
-/*////////////////////////////////*/
+import { areJidsSameUser } from '@adiwajshing/baileys'
 
 let handler = async (m, { conn, text, participants, groupMetadata }) => {
 	if(isNaN(text)) {
@@ -26,7 +21,7 @@ let handler = async (m, { conn, text, participants, groupMetadata }) => {
 			}  
 		} catch (e) {
   } finally {
-    let users = m.isGroup ? participants.find(u => u.jid == user) : {}
+    let users = m.isGroup ? participants.find(v => areJidsSameUser(v.jid, user)) : {}
     if(!users) return conn.reply(m.chat, `Target atau Nomor tidak ditemukan, mungkin sudah keluar atau bukan anggota grup ini.`, m)
     if(user === m.sender) return conn.reply(m.chat, `Tidak bisa berpacaran dengan diri sendiri!`, m)
     if(user === conn.user.jid) return conn.reply(m.chat, `*Tidak bisa berpacaran dengan saya t_t`, m)
