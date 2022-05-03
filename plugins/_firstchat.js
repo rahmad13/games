8dd5f4e967708f845937d29b220fab30578e5129
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-05-03 14:52:50 +0700

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -1,12 +1,12 @@
 
 
 export async function all(m) {
-if (m.isBaileys || m.fromMe || m.isGroup) return !0
-let user = global.db.data.users[m.sender]
+if (m.chat.endsWith('broadcast') || m.fromMe || m.isGroup || db.data.settings[this.user.jid].group) return
+   let user = global.db.data.users[m.sender]
     const cooldown = 86400000
     if (new Date - user.pc < cooldown) return // setiap 24 jam sekali
     await this.sendButton(m.chat, `
-Hai, ${user.banned ? 'kamu dibanned' : `Ada yang bisa ${this.user.name} bantu?`}
-`.trim(), author, [[user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : ',?']], m)
+Hai ${conn.getName(m.sender)}, ${user.banned ? 'kamu dibanned' : `Ada yang bisa ${this.user.name} bantu?\nKetik */menu* untuk melihat list fitur bot`}
+`.trim(), author, [[user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? '.owner' : '/menu']], m)
     user.pc = new Date * 1
 }
