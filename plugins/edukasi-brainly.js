82c4767c60046a4104ea7a1be1e65adbccf77086
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-05-03 17:10:14 +0700

    Create edukasi-brainly.js

---
+++ plugins/edukasi-brainly.js
@@ -0,0 +1,16 @@
+import Brainly from 'brainly-scraper-v2'
+const brain = new Brainly('id')
+
+let handler = async function (m, { text, usedPrefix, command }) {
+  if (!text) throw `Pengunaan:\n${usedPrefix + command} <soal>\n\nContoh:\n${usedPrefix + command} apa itu javascript?`
+  brain.search('id', text).then(res => {
+    let json = JSON.parse(JSON.stringify(res))
+    let answer = json.map((v, i) => `_*PERTANYAAN KE ${i + 1}*_\n${v.question.content}\n${v.answers.map((v, i) => `*JAWABAN KE ${i + 1}*\n${v.content.replace(/<\/?p>|<\/?strong>|<\/?u>|<\/?h[1-3]>|<\/?span>/g, '').replace(/<br ?(\/|\\)?>/g, '\n')}`).join('\n')}`).join('\n\n•------------•\n\n')
+    m.reply(answer)
+  })
+}
+handler.help = ['brainly <soal>']
+handler.tags = ['edukasi']
+handler.command = /^brainly$/i
+
+export default handler
