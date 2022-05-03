1cb42572c8bafd3d3b194aadbeea21f0d46555bb
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-05-03 14:41:19 +0700

    Update audio-changer.js
    fx

--- plugins/audio-changer.js
+++ plugins/audio-changer.js
@@ -20,8 +20,8 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         if (/blown/.test(command)) set = '-af acrusher=.1:1:64:0:log'
         if (/deep/.test(command)) set = '-af atempo=4/4,asetrate=44500*2/3'
         if (/earrape/.test(command)) set = '-af volume=12'
-        if (/fast/.test(command)) set = '-filter:a "atempo=1.63,asetrate=44100"'
-        if (/fat/.test(command)) set = '-filter:a "atempo=1.6,asetrate=22100"'
+        if (/fast/.test(command)) set = `-filter:a "atempo=1.63,asetrate=${args[0]}"`
+        if (/fat/.test(command)) set = `-filter:a "atempo=1.6,asetrate=22100"`
         if (/nightcore/.test(command)) set = '-filter:a atempo=1.06,asetrate=44100*1.25'
         if (/reverse/.test(command)) set = '-filter_complex "areverse"'
         if (/robot/.test(command)) set = '-filter_complex "afftfilt=real=\'hypot(re,im)*sin(0)\':imag=\'hypot(re,im)*cos(0)\':win_size=512:overlap=0.75"'
