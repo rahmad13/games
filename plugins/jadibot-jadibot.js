9fd7a80740df9129502248706015a145b4e981fb
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-05-04 11:27:36 +0700

    Update jadibot-jadibot.js

--- plugins/jadibot-jadibot.js
+++ plugins/jadibot-jadibot.js
@@ -1,17 +1,75 @@
-import qrcode = require('qrcode')
-import { getAuthenticationCredsType } from "@adiwajshing/baileys"
+import qrcode from 'qrcode'
+import store from '../lib/store.js'
 
 if (global.conns instanceof Array) console.log()// for (let i of global.conns) global.conns[i] && global.conns[i].user ? global.conns[i].close().then(() => delete global.conns[id] && global.conns.splice(i, 1)).catch(global.conn.logger.error) : delete global.conns[i] && global.conns.splice(i, 1)
 else global.conns = []
 
 let handler  = async (m, { conn, args, usedPrefix, command }) => {
-  m.reply('in development')
+  let parent = args[0] && args[0] == 'plz' ? conn : global.conn
+  let auth = false
+  if ((args[0] && args[0] == 'plz') || global.conn.user.jid == conn.user.jid) {
+    let id = global.conns.length
+    let conn = new global.conn.constructor()
+    if (args[0] && args[0].length > 200) {
+      let json = Buffer.from(args[0], 'base64').toString('utf-8')
+      // global.conn.reply(m.isGroup ? m.sender : m.chat, json, m)
+      let obj = JSON.parse(json)
+      await store.useSingleFileAuthState(obj)
+      auth = true
+    }
+    conn.on('qr', async qr => {
+      let scan = await parent.sendFile(m.chat, await qrcode.toDataURL(qr, { scale: 8 }), 'qrcode.png', 'Scan QR ini untuk jadi bot sementara\n\n1. Klik titik tiga di pojok kanan atas\n2. Ketuk Perangkat Tertaut\n3. Scan QR ini \nQR Expired dalam 20 detik', m)
+      setTimeout(() => {
+        parent.deleteMessage(m.chat, scan.key)
+      }, 30000)
+    })
+    conn.welcome = global.conn.welcome + ''
+    conn.bye = global.conn.bye + ''
+    conn.spromote = global.conn.spromote + ''
+    conn.sdemote = global.conn.sdemote + ''
+    conn.handler = global.conn.handler.bind(conn)
+    conn.onDelete = handler.deleteUpdate.bind(global.conn)
+    conn.participantsUpdate = handler.participantsUpdate.bind(global.conn)
+    //
+    conn.on('messages.upsert', conn.handler)
+    conn.on('group-participants.update', conn.participantsUpdate)
+    conn.on('message.delete', conn.onDelete)
+  
+    conn.connect().then(async ({user}) => {
+      parent.reply(m.chat, 'Berhasil tersambung dengan WhatsApp - mu.\n*NOTE: Ini cuma numpang*\n' + JSON.stringify(user, null, 2), m)
+      if (auth) return
+      await parent.reply(user.jid, `Kamu bisa login tanpa qr dengan pesan dibawah ini. untuk mendapatkan kode lengkapnya, silahkan kirim *${usedPrefix}getcode* untuk mendapatkan kode yang akurat`, m)
+      parent.reply(user.jid, `${usedPrefix + command} ${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}`, m)
+    })
+    setTimeout(() => {
+      if (conn.user) return
+      conn.close()
+      let i = global.conns.indexOf(conn)
+      if (i < 0) return
+      delete global.conns[i]
+      global.conns.splice(i, 1)
+    }, 60000)
+    conn.on('close', () => {
+      setTimeout(async () => {
+        try {
+          if (conn.state != 'close') return
+          if (conn.user && conn.user.jid)
+            parent.reply(conn.user.jid, `Koneksi terputus...`, m)
+          let i = global.conns.indexOf(conn)
+          if (i < 0) return
+          delete global.conns[i]
+          global.conns.splice(i, 1)
+        } catch (e) { conn.logger.error(e) }
+      }, 30000)
+    })
+    global.conns.push(conn)
+  } else throw 'Tidak bisa membuat bot didalam bot!\n\nhttps://wa.me/' + global.conn.user.jid.split`@`[0] + '?text=.jadibot'
 }
 handler.help = ['jadibot']
 handler.tags = ['jadibot']
 
 handler.command = /^jadibot$/i
 
-handler.limit = false
+handler.limit = 1
 
 export default handler
