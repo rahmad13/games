b4b80b0b88398dc186b3e48a466fd7f05cf93dff
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-05-04 07:49:15 +0700

    Update downloader-tiktokmusik.js

--- plugins/downloader-tiktokmusik.js
+++ plugins/downloader-tiktokmusik.js
@@ -1,10 +1,11 @@
-import { musicaldown } from '../lib/tiktokdl.js'
+import { tiktokmusic } from '../lib/scrapes.js'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    const { result } = await musicaldown(args[0])
-    const done = result.audio || result.audio_original
+    const { meta } = await tiktokmusic(args[0])
+    const done = meta.music.playUrl
     if (!done) throw 'Can\'t download audio!'
+    m.reply(wait)
     conn.sendFile(m.chat, done, 'tiktok.mp3', null, m)
 }
 handler.help = ['tiktokmusik'].map(v => v + ' <url>')
