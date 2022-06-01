371907f91ba308b92022e2e6674af5d08acf0c8e
Rhmd <93415263+rahmad13@users.noreply.github.com>
2022-06-02 03:10:16 +0700

    Update rpg-mission.js

--- plugins/rpg-mission.js
+++ plugins/rpg-mission.js
@@ -2,57 +2,101 @@
 var misi = [
   {
     "index": 1,
-    "rank": "S++",
+    "rank": "SSS",
     "misii": "Menaklukan Raja Iblis",
     "exp": 97838,
-    "crystal": 37
+    "healt": 100,
+    "stamina": 120,
+    "crystal": 37,
+    "title": "Penakluk raja iblis",
+    "point": 28,
+    "gems": 10
   },
   {
     "index": 2,
     "rank": "F",
     "misii": "Mencari Crystal",
+    "healt": 0,
+    "stamina": 60,
     "exp": 1072,
-    "crystal": 3
+    "crystal": 3,
+    "title": "crystal hunter",
+    "point": 0,
+    "gems": 0
   },
   {
     "index": 3,
-    "rank": "S+",
+    "rank": "SS+",
     "misii": "Taklukan naga Everest",
     "exp": 79938,
-    "crystal": 24
+    "healt": 90,
+    "stamina": 90,
+    "crystal": 24,
+    "title": "Sang Penakluk Naga Everest",
+    "point": 12,
+    "gems": 9
   },
   {
     "index": 4,
     "rank": "B+",
     "misii": "Memusnahkan Desa Goblin",
     "exp": 5647,
-    "crystal": 9
+    "healt": 40,
+    "stamina": 80,
+    "crystal": 9,
+    "title": "Musuh Para Goblin",
+    "point": 3,
+    "gems": 1
   },
   {
     "index": 5,
     "rank": "C+",
     "misii": "Menyelesaikan Dungeon Berisi Monster Laba laba",
     "exp": 4837,
-    "crystal": 7
+    "healt": 74,
+    "stamina": 86,
+    "crystal": 7,
+    "title": "spider monster killer",
+    "point": 2,
+    "gems": 1
   },
   {
     "index": 6,
     "rank": "A+",
     "misii": "Membuat Ramuan Penyembuh penyakit 50 poison",
+    "healt": 2,
+    "stamina": 79,
     "exp": 26829,
-    "crystal": 15
+    "crystal": 15,
+    "title": "Alkemis Ramuan Hebat",
+    "point": 10,
+    "gems": 2
+  },
+ {
+    "index": 7,
+    "rank": "E+",
+    "misii": "Memungut kristal di dungeon",
+    "exp": 3000,
+    "healt": 3,
+    "stamina": 50,
+    "crystal": 5,
+    "title": "crystal picker",
+    "point": 0
   }
 ]
 
 //Class
-async function handler(m, { conn, args, usedPrefix, command }) {
-  conn.mission = conn.mission || {}
+async function handler(m, { conn, args, text , usedPrefix, command }) {
+  //conn.mission = conn.mission || {}
+  conn.mission = conn.mission ? conn.mission : {}
   if(m.sender in conn.mission) return m.reply("Kamu masih melakukan misi, tunggu sampai selesai!!")
 
   try {
     let json = misi[Math.floor(Math.random() * misi.length)] //get misi
     const cooldown = 5 * (1000 * 60) //coldown timer second
     let user = global.db.data.users[m.sender] //Get db user
+    
+    if (user.health == 0 || user.stamina == 0 || user.mana == 0) return m.reply(`your stamina/healt/mana is less than 100`)
 
     if(typeof user.lastmisi != "number") global.db.data.users[m.sender].lastmisi = 0
     if(typeof user.exp != "number") global.db.data.users[m.sender].exp = 0
@@ -69,18 +113,17 @@ async function handler(m, { conn, args, usedPrefix, command }) {
         json
       }
       //Caption
-      let caption = `*A MISSION HAS BEEN GIVEN TO THE HUNTER!*
+      let caption = `*A MISSION HAS BEEN GIVEN TO THE HUNTER! (BETA)*
 *🥇 RANK:* ${json.rank}
 *📰 MISI:* ${json.misii}
 *🎁 GIFT:* Exp ${json.exp} & Crystal Mana ${json.crystal}
+${ json.title ? `*🔖 TITLE:* ${json.title}` : '\n'} ${json.gems ? `Gems: ${json.gems}` : `\n`}
 
-<!>UNREALESE FEATURE<!>
-
-- Party <Room>
-- Solo (solo misi)
+slect option
+- Accept
 - Cancel
 `
-      return conn.sendButton(m.chat, caption, author, null, [['Create Party', 'cparty'], ['Solo','solo'], ['Cancel','cancel']], m) //SendMessage
+      return conn.sendButton(m.chat, caption, author, null, [['Accept','accept'], ['Cancel','cancel']], m) //SendMessage
     }
   } catch (e) {
     console.error(e)
@@ -94,7 +137,8 @@ async function handler(m, { conn, args, usedPrefix, command }) {
 }
 
 handler.before = async m => {
-  conn.mission = conn.mission || {}
+  //conn.mission = conn.mission || {}
+  conn.mission = conn.mission ? conn.mission : {}
   if(!(m.sender in conn.mission)) return
   if(m.isBaileys) return
 
@@ -103,7 +147,7 @@ handler.before = async m => {
   let user = global.db.data.users[m.sender] //Get db user
 
   let txt = (m.msg && m.msg.selectedDisplayText ? m.msg.selectedDisplayText : m.text ? m.text : '').toLowerCase()
-  if(txt != "cparty" && txt != "solo" && txt != "cancel") return
+  if(txt != "accept" && txt != "cancel" && txt != "gas") return
 
   if(typeof user.lastmisi != "number") global.db.data.users[m.sender].lastmisi = 0
   if(typeof user.exp != "number") global.db.data.users[m.sender].exp = 0
@@ -122,20 +166,26 @@ handler.before = async m => {
 
   //Gacha systemBeta
   try {
-    if(/^solo?$/i.test(txt)) {
+    if(/^accept?$/i.test(txt)) {
       if(Aku > Kamu) {
-        var cpt = `Berhasil Menyelesaikan misi ${json.misii}`
+        var cpt = `Berhasil Menyelesaikan 📰misi ${json.misii}`
+        m.reply(`${json.title ? `You got a title ${json.title}`: ""}`)
         m.reply(cpt)
         user.exp += json.exp
         user.crystal += json.crystal
-      } else if(Kamu < Aku) {
-        var flr = `Gagal Menyelesaikan Misi ${json.misii} Dikarenakan ${aui} `
+        user.title += json.title
+        user.misi += json.misii
+      } else if(Aku < Kamu) {
+        var flr = `Gagal Menyelesaikan 📰Misi ${json.misii} Dikarenakan ${aui} `
         m.reply(flr)
       } else {
-        var cpt = `Berhasil Menyelesaikan misi ${json.misii}`
-        m.reply(cpt)
+        var cpe = `Berhasil Menyelesaikan 📰misi ${json.misii}`
+        m.reply(`${json.title ? `You got a title ${json.title}`: ""}`)
+        m.reply(cpe)
         user.exp += json.exp
         user.crystal += json.crystal
+        user.title += json.title
+        user.misi += json.misii
       }
       user.lastmisi = new Date * 1
       clearTimeout(timeout)
